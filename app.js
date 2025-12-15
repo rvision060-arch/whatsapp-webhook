c0a17416c84f27c179996b6db0f5425410068835
rvision060-arch <rvision060@gmail.com>
2025-12-15 15:13:50 +0200

    Update app.js

--- app.js
+++ app.js
@@ -26,4 +26,6 @@ app.post("/webhook", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server running on port", PORT));
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
